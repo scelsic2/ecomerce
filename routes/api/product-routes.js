6add6a542f75aa02f1e6390f37e5c71243d3dc5d
scelsic2 <scelsic2@gmail.com>
2023-04-16 12:23:18 -0400

    GET products routes work

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,13 +4,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const products = await Product.findAll({
+    include: [
+      {model: Category},
+      {model: Tag}
+    ]
+  })
+  res.send(products)
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  const product_id = req.params.id;
+  const include_cat = req.query.product_id;
+
+  if(product_id) {
+    const product = await Product.findOne({
+      include: Category,
+      where: {
+        id: product_id
+      },
+      include: [
+        {model: Category},
+        {model: Tag}
+      ]
+    })
+    res.send(product);
+  } else res.send ("Error") 
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
